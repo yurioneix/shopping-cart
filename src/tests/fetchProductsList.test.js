1cbc25f359611e828074eedfc81b20ab3b5a4127
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-11-16 20:40:15 -0300

    Requisito 1: testa se o retorno da função fetchProductsList passando o argumento 'computador' é um objeto igual ao computadorSearch

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -4,21 +4,25 @@ import computadorSearch from './mocks/search';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
+  beforeEach(async () => {
+    await fetchProductsList('computador');
+  });
+
   it('fetchProductsList é uma função', () => {
     expect(typeof fetchProductsList).toBe('function');
   });
 
   it('fetch é chamado ao executar fetchProductsList', async () => {
-   await fetchProductsList('computador');
    expect(fetch).toHaveBeenCalledTimes(1);
   });
 
   it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
-    await fetchProductsList('computador')
     const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
     expect(fetch).toHaveBeenCalledWith(endpoint);
   });
 
-  // it('...', () => {
-  // });
+  it('retorno da função fetchProductsList com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    const response = await fetchProductsList('computador');
+    expect(response.results).toEqual(computadorSearch)
+  });
 });
