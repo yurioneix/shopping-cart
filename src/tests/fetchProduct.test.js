67b75bd884d92495ecb316607d81bd0e4c2a1000
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-11-18 15:28:00 -0300

    Requisito 5: testa se a função fetch utiliza o endpoint correto(falha esperada)

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -7,4 +7,8 @@ describe('Teste a função fetchProduct', () => {
   it('é uma função', () => {
     expect(typeof fetchProduct).toBe('function');
   });
+  it('o chamar a função fetchProduct com o argumento do produto "MLB1405519561", a função fetch utiliza o endpoint correto', () => {
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1405519561';
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
 });
