fd11cfa9909e6578a7ad75c8426aabcc77b5f214
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-11-18 15:35:09 -0300

    Requisito 5: testa se o retorno da função fetchProduct com um id como argumento é igual a estrutura de dados do objeto product

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -7,9 +7,14 @@ describe('Teste a função fetchProduct', () => {
   it('é uma função', () => {
     expect(typeof fetchProduct).toBe('function');
   });
+
   it('o chamar a função fetchProduct com o argumento do produto "MLB1405519561", a função fetch utiliza o endpoint correto', async () => {
     await fetchProduct('MLB1405519561');
     const endpoint = 'https://api.mercadolibre.com/items/MLB1405519561';
     expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+
+  it('se o retorno da função fetchProduct com o argumento do produto "MLB1405519561" é uma estrutura de dados igual ao objeto produto', async () => {
+   await expect(fetchProduct("MLB1405519561")).toEqual(product);
   });
 });
