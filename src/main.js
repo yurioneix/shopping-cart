24733219630a985b2535275268ca9c4ba3d02f26
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-11-18 10:56:15 -0300

    Requisito 3: testando

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
+/* */
 document.querySelector('.cep-button').addEventListener('click', searchCep);
-//teste
 const sectionProducts = document.querySelector('.products');
 const request = await fetchProductsList('computador');
 request.forEach((product) => sectionProducts.appendChild(createProductElement(product)));
