4e5b71da58bc17f40c089d026980ad9ab93729a7
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-11-18 10:35:56 -0300

    Requisito 3: resolvido

--- src/main.js
+++ src/main.js
@@ -1,15 +1,10 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-// console.log();
-fetchProductsList('computador')
-  .then((data) => data.forEach((product) => console.log(product)));
-
-// const { id, title, thumbnail, price } = fetchProductsList('computador');
-// console.log(id);
-// console.log(title);
-// console.log(thumbnail);
-// console.log(price);
+const sectionProducts = document.querySelector('.products');
+const request = await fetchProductsList('computador');
+request.forEach((product) => sectionProducts.appendChild(createProductElement(product)));
