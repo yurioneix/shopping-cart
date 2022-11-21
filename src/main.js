a5f02b9d5d5395013d7029543e6d0c3a1b60828e
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-11-21 11:47:29 -0300

    Requisito 9: resolvido

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList, fetchProduct } from './helpers/fetchFunctions';
 import { createProductElement, createCartProductElement } from './helpers/shopFunctions';
-import { saveCartID } from './helpers/cartFunctions';
+import { saveCartID, getSavedCartIDs } from './helpers/cartFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
@@ -9,7 +9,6 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 const sectionProducts = document.querySelector('.products');
 const sectionProductsAll = document.querySelectorAll('.products');
 const container = document.querySelector('.container');
-const productsButton = document.querySelector('.product__add');
 const cartProducts = document.querySelector('.cart__products');
 
 const createLoading = () => {
@@ -48,5 +47,11 @@ sectionProductsAll.forEach((section) => section
       cartProducts.appendChild(resultElement);
     }
   }));
+
+// console.log(getSavedCartIDs().map(async (id) => Promise.all([await fetchProduct(id)])))
+const getLocalStorage = getSavedCartIDs().map(async (id) => fetchProduct(id));
+const storeProducts = await Promise.all(getLocalStorage);
+storeProducts
+  .map((product) => cartProducts.appendChild(createCartProductElement(product)));
 
 createProductList();
